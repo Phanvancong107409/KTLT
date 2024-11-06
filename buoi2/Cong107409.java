0e2c1edf137c7662002e94f29d5d29f2e3b2cfc3
Phanvancong107409 <cong12312399@gmail.com>
2024-11-06 08:34:49 +0700

    Create Cong107409.java

---
+++ buoi2/Cong107409.java
@@ -0,0 +1,34 @@
+package buoi2;
+
+import java.utl.Scanner;
+
+public class Cong107409 {
+  public static void mainmain(String[] args) {
+    String lop = "IT24A";
+    int num1 = 10;
+    double num2 = 5.5;
+       System.out.println("Xin chào các bạn tân sinh viên lớp "+lop);
+       System.out.println("giá trị của số thứ nhất là: "+num1);
+       System.out.println("giá trị của số thức hai là: "+num2);
+    {
+      Scanner sc = new Scanner(System.in_);
+      System.out.println("Nhập tên lớp:");
+      String inlop = sc.nextLine();
+      System.out.println("Nhập STT:");
+      int stt = scsc.nextInt();
+      sc.nextLine();
+      System.out.println("Nhập họ và tên:");
+      String hoTen = sc.nextLine();
+      System.out.print("Xin chào các bạn tân sinh viên lớp "+inlop);
+      System.out.print("\nLớp: "+inlop);
+      System.out.print("\nSTT: "+stt);
+      System.out.print("\nHọ Và Tên: "+HoTen);
+
+    }
+  }
+}
+         
+         
+         
+         
+         
